82c0dca643b35c4679d50540dd978bd1d5db400e
joacoalbarracin <joacoalbarracin@gmail.com>
2023-06-26 22:15:36 -0300

    Agregue el multer

--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -4,6 +4,20 @@ const router = express.Router();
 
 const controller = require('../controllers/mainController');
 
+const path = require('path')
+
+const multer = require('multer')
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.resolve(__dirname, '../../public/img'))
+    },
+    filename: (req, file, cb) => {
+        let imageName = Date.now() + path.extname(file.originalname);
+        cb(null, imageName);
+    }
+})
+const fileUpload = multer({storage: storage})
+
 router.get('/', controller.home);
 router.get('/login', controller.login);
 router.get('/productCart', controller.productCart);
@@ -13,7 +27,7 @@ router.get('/productList', controller.productList);
 
 //FORM EDIT
 router.get('/editProduct/:id', controller.editProduct)
-router.put('/editProduct/:id', controller.processEdit);
+router.put('/editProduct/:id', fileUpload.single('image'), controller.processEdit);
 
 //DELETE
 router.delete('/delete/:id', controller.deleteProduct)
