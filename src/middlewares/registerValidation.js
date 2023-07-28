30fe84febf24f0086b8eed0c7c6dd5a088377a2f
joacoalbarracin <joacoalbarracin@gmail.com>
2023-07-27 22:29:18 -0300

    se agrega una validacion a la validacion de registro

--- src/middlewares/registerValidation.js
+++ src/middlewares/registerValidation.js
@@ -5,4 +5,5 @@ module.exports = [
     body('last_name').notEmpty().withMessage('Indica tu apellido'),
     body('email').isEmail().withMessage('Debes indicar un email válido'),
     body('password').isStrongPassword({minLenght: 8, minNumbers:1, minUpperCase:1, minLowerCase: 1, minSymbols:1}).withMessage('La contraseña debe tener mínimo 8 caracteres. Debe contener una mayúscula, una minúscula, un número y un caracter especial.'),
+    body('opcion').notEmpty().withMessage('Debes elegir entre ser Comprador o Administrador')
 ]
