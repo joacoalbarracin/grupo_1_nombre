ab3873ec21195d15688c221de71fe9deb69bd8d6
mayzevg <zevaoglum@gmail.com>
2023-08-26 20:02:21 -0300

    quitar log middleware

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const mainRouter = require('./routes/mainRouter'); // Requerimos el módulo main
 const userRouter = require('./routes/userRouter'); // Requerimos el módulo userRouter
 const methodOverride = require('method-override'); // Requerimos el módulo method-override
 const session = require('express-session'); // Requerimos el módulo express-session
-const logs = require('./middlewares/logs') //Traemos el middleware del log
+
 const cookie = require('cookie-parser')
 
 app.use(express.urlencoded({extended: false})); // Para leer lo que llega en el req.body
@@ -18,7 +18,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false
 })); // Para poder usar las sesiones
-app.use(logs)
+
 
 
 app.set('views', path.join(__dirname,'../views')); // Para indicar la ruta de la carpeta de las vistas
