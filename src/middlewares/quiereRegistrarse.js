ac3e7a2eee0d2d43368a15ba3c30654a79dba69b
unknown <you@example.com>
2023-09-20 22:33:08 -0300

    responsive banner

--- src/middlewares/quiereRegistrarse.js
+++ src/middlewares/quiereRegistrarse.js
@@ -1,10 +1,23 @@
-const quiereRegistrarse = (req,res,next) => {
-    if (req.session.usuarioLogueado) {
-        return res.redirect ('/')
-    }
-    else {
-        next()
-    }
-}
+const { body } = require('express-validator');
+const User = require('../database/models/User'); // importar el modelo de usuario
 
-module.exports = quiereRegistrarse
+module.exports = [
+  body('email')
+    .notEmpty().withMessage('Debes indicar tu email')
+    .isEmail().withMessage('El email ingresado no es válido'),
+  body('password')
+    .notEmpty().withMessage('Debes indicar tu contraseña')
+    .isLength({ min: 8 }).withMessage('La contraseña debe tener al menos 8 caracteres')
+    .custom((value, { req }) => {
+      return User.findOne({ email: req.body.email }).then(user => {
+        if (!user) {
+          return Promise.reject('El correo electrónico ingresado no está registrado');
+        }
+        return user.comparePassword(value).then(valid => {
+          if (!valid) {
+            return Promise.reject('La contraseña ingresada es incorrecta');
+          }
+        });
+      });
+    })
+];
