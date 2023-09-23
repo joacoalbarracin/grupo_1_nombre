d0dec663d68c0fc2e62dee76c8eb6394ef28c68a
jnieva95 <81454378+jnieva95@users.noreply.github.com>
2023-09-23 14:35:03 -0300

    Add files via upload
    Validaciones

--- public/js/editProducts.js
+++ public/js/editProducts.js
@@ -8,20 +8,18 @@ document.addEventListener('DOMContentLoaded', function () {
         const name = document.querySelector('#name');
         const description = document.querySelector('#description');
         const image = document.querySelector('#image');
-        const date = document.querySelector('#date'); // Agregamos el campo de fecha
+        const date = document.querySelector('#date'); 
 
         const nameError = document.querySelector('#name-error');
         const descriptionError = document.querySelector('#description-error');
         const imageError = document.querySelector('#image-error');
-        const dateError = document.querySelector('#date-error'); // Agregamos el campo de fecha
+        const dateError = document.querySelector('#date-error'); 
 
-        // Limpiar mensajes de error anteriores
-        nameError.textContent = '';
-        descriptionError.textContent = '';
-        imageError.textContent = '';
-        dateError.textContent = ''; // Limpiamos el mensaje de error de fecha
+        nameError.textContent = ''; // Limpiar mensajes de error anteriores
+        descriptionError.textContent = '';// Limpiar mensajes de error anteriores
+        imageError.textContent = '';// Limpiar mensajes de error anteriores
+        dateError.textContent = ''; // Limpiar mensajes de error anteriores
 
-        // Validación para Nombre
         if (name.value.trim() === '') {
             errores.push('El campo Nombre es obligatorio.');
             nameError.textContent = 'El campo Nombre es obligatorio.';
@@ -30,13 +28,11 @@ document.addEventListener('DOMContentLoaded', function () {
             nameError.textContent = 'El campo Nombre debe tener al menos 5 caracteres.';
         }
 
-        // Validación para Descripción
         if (description.value.trim().length < 20) {
             errores.push('El campo Descripción debe tener al menos 20 caracteres.');
             descriptionError.textContent = 'El campo Descripción debe tener al menos 20 caracteres.';
         }
 
-        // Validación para Imagen
         if (image.files.length === 0) {
             errores.push('Debe seleccionar una imagen.');
             imageError.textContent = 'Debe seleccionar una imagen.';
@@ -51,7 +47,6 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
 
-        // Validación para Fecha (no en el pasado)
         const currentDate = new Date();
         const selectedDate = new Date(date.value);
 
